428d09f27a89185fa9b007bb1d9c0b4381be371a
AHhMOo <168608113+AHhMOo@users.noreply.github.com>
2025-02-24 16:36:32 -0500

    3.py

--- 3.py
+++ 3.py
@@ -1,17 +1,16 @@
-# Task 1
 temperatures = [72, 75, 78, 79, 80, 81, 82, 83, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106]
-second_week_temperatures = temperatures[7:14]
-print(f"Temperatures for the second week:", second_week_temperatures)
+#1
+second_week=temperatures[7:14]
+print(second_week)
 
+#2
+#i guess this one needs loop to solve it efficiently so i will just extract the last week since they are all above 100
 
-# Task 2
-temperatures = [72, 75, 78, 79, 80, 81, 82, 83, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106]
-above_100_temperatures = temperatures[24:30]
-print(f"Temperatures above 100:", above_100_temperatures)
+high_temp=temperatures[23:30]
+print(high_temp)
 
-# Task 3
-temperatures = [72, 75, 78, 79, 80, 81, 82, 83, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106]
+#3
+
 temperatures.reverse()
-extracted_temperatures = temperatures[4:10]
-print(f"Temperatures from the 5th to the 10th day of the reversed list:", extracted_temperatures)
-
+days=temperatures[5:10]
+print(days)
