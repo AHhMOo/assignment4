27a17b35adfb5f12c8207d70e5f30f9fe4501d49
AHhMOo <168608113+AHhMOo@users.noreply.github.com>
2025-02-25 01:50:28 -0500

    3.py

--- 3.py
+++ 3.py
@@ -1,16 +1,15 @@
 temperatures = [72, 75, 78, 79, 80, 81, 82, 83, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106]
-#1
-second_week=temperatures[7:14]
-print(second_week)
 
-#2
-#i guess this one needs loop to solve it efficiently so i will just extract the last week since they are all above 100
 
-high_temp=temperatures[23:30]
-print(high_temp)
+second_week = temperatures[7:15]
 
-#3
+print("Second week temperatures:", ", ".join(map(str, second_week)))
 
-temperatures.reverse()
-days=temperatures[5:10]
-print(days)
+
+above_100 = []
+
+for temp in temperatures:
+    if temp > 100:
+        above_100.append(temp)
+
+print("Temperatures above 100:", ", ".join(map(str, above_100)))
