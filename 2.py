aad531189126f383734e1290ea563b28516b2783
AHhMOo <168608113+AHhMOo@users.noreply.github.com>
2025-02-25 01:48:05 -0500

    2.py

--- 2.py
+++ 2.py
@@ -1,29 +1,12 @@
 submitted = ["Alice", "Bob", "Charlie", "David"]
 attended = ["Charlie", "Eve", "Alice", "Frank"]
 #1
-if attended[0] in submitted :
-    print(attended[0]+" has submitted their assignments and attended the class")
-if attended[1] in submitted :
-    print(attended[1]+" has submitted their assignments and attended the class")
-if attended[2] in submitted :
-    print(attended[2]+" has submitted their assignments and attended the class")
-if attended[3] in submitted :
-    print(attended[3]+" has submitted their assignments and attended the class")
+submitted = ["Alice", "Bob", "Charlie", "David"]
+attended = ["Charlie", "Eve", "Alice", "Frank"]
 
-#2
+if "Alice" in submitted and "Alice" in attended:
+    print("Alice submitted the assignment and attended class.")
+else:
+    print("Alice did not submit the assignment and/or did not attend class.")
 
-submitted.sort()
-attended.sort()
-if submitted == attended :
-    print("lists are identical")
 
-#3
-if attended[3] not in submitted :
-    attended.remove(attended[3])
-if attended[2] not in submitted :
-    attended.remove(attended[2])
-if attended[1] not in submitted :
-    attended.remove(attended[1])
-if attended[0] not in submitted :
-    attended.remove(attended[0])
-print(attended)
