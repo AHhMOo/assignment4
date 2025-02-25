e3a3b014676185b88d190257ead34df558c58b4f
AHhMOo <168608113+AHhMOo@users.noreply.github.com>
2025-02-25 01:46:28 -0500

    1.py

--- 1.py
+++ 1.py
@@ -5,29 +5,12 @@ grades.reverse()
 print(grades)
 
 #2
-average = (grades[0]+grades[1]+grades[2]+grades[3]+grades[4]+grades[5]+grades[6]+grades[7]+grades[8]+grades[9]) // 10
-print(average)
 
-#3
-if grades[0]>80 :
-    grades[0] = "failed"
-if grades[1]>80 :
-    grades[1] = "failed"
-if grades[2]>80 :
-    grades[2] = "failed"
-if grades[3]>80 :
-    grades[3] = "failed"
-if grades[4]>80 :
-    grades[4] = "failed"
-if grades[5]>80 :
-    grades[5] = "failed"
-if grades[6]>80 :
-    grades[6] = "failed"
-if grades[7]>80 :
-    grades[7] = "failed"
-if grades[8]>80 :
-    grades[8] = "failed"
-if grades[9]>80 :
-    grades[9] = "failed"
+total = 0
 
-print (grades)
+for grade in grades:
+    total += grade
+
+average = total / len(grades)
+
+print(f"The average grade is: {average:.2f}")
