83ed02655ca6b35058f3e586cce2ce9475fade2d
AHhMOo <168608113+AHhMOo@users.noreply.github.com>
2025-02-25 01:48:20 -0500

    2.py

--- 2.py
+++ 2.py
@@ -1,8 +1,6 @@
 submitted = ["Alice", "Bob", "Charlie", "David"]
 attended = ["Charlie", "Eve", "Alice", "Frank"]
 #1
-submitted = ["Alice", "Bob", "Charlie", "David"]
-attended = ["Charlie", "Eve", "Alice", "Frank"]
 
 if "Alice" in submitted and "Alice" in attended:
     print("Alice submitted the assignment and attended class.")
